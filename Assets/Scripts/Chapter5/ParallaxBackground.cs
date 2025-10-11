4eaf127430f80c0d4a090539c6227eb0eba4d19f
LuanS1mple <luans1mple2004@gmail.com>
2025-10-11 11:57:05 +0700

    Revert "Merge backup into finaldevelop: take theirs (backup)"
    This reverts commit d8744126c63a17b64401e3379b8bba257ff0b058, reversing
    changes made to f9af61eb672a3136022cadc6c9d8b207b2698ebf.

--- Assets/Scripts/Chapter5/ParallaxBackground.cs
+++ Assets/Scripts/Chapter5/ParallaxBackground.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ParallaxBackground : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform cameraTransform;
+    public float parallaxMultiplier = 0.3f;
+    private Vector3 lastCameraPosition;
+
     void Start()
     {
-        
+        lastCameraPosition = cameraTransform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate() // phải là LateUpdate!
     {
-        
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += new Vector3(deltaMovement.x * parallaxMultiplier, deltaMovement.y * parallaxMultiplier, 0);
+        lastCameraPosition = cameraTransform.position;
     }
 }
