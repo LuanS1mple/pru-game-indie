f3a9fd7120e7dabfcb7f2c2bc54f1cbb447efdc3
Khoa2305 <dikhoadiem@gmail.com>
2025-10-16 17:13:28 +0700

    Commit one

---
+++ Assets/Scripts/MapCave2/Monster2Controller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster2Controller : MonoBehaviour
+{
+    public Transform gunTip;
+    public GameObject bullet;
+    public float fireRate = 0.5f;   
+    private float nextFire = 0f;
+
+    void Update()
+    {
+        AutoFire();
+    }
+
+    private void AutoFire()
+    {
+        if (Time.time > nextFire)
+        {
+
+            nextFire = Time.time + fireRate;
+            Instantiate(bullet, gunTip.position, Quaternion.Euler(new Vector3(0, 0, 180)));
+        }
+    }
+}
