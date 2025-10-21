502c207532d9245325d49247d71d8567cbff3652
LuanS1mple <luans1mple2004@gmail.com>
2025-10-21 11:33:30 +0700

    block door boss and open by key

---
+++ Assets/Scripts/Chapter6/KeyControll/KeyDoorController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyDoorController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int keyCount = 0; // Biến đếm số key
+    public GameObject door;  // Cửa cần mở
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Khi va chạm với vật có tag "Chapter6-Key"
+        if (collision.CompareTag("Chapter6-Key"))
+        {
+            keyCount++;
+            Debug.Log("Đã nhặt chìa khóa: " + keyCount);
+
+            // Xóa chìa khóa sau khi nhặt
+            Destroy(collision.gameObject);
+
+            // Nếu đã đủ 3 chìa khóa
+            if (keyCount >= 3 && door != null)
+            {
+                Collider2D doorCollider = door.GetComponent<Collider2D>();
+                if (doorCollider != null)
+                {
+                    doorCollider.isTrigger = true; // Mở cửa
+                    Debug.Log("Cửa đã mở!");
+                }
+            }
+        }
+    }
+}
