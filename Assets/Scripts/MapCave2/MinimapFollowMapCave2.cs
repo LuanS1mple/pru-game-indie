8c38e297a14b00b3b525dcd6552fb4db4a8557fb
Khoa2305 <dikhoadiem@gmail.com>
2025-10-24 17:11:23 +0700

    Update

---
+++ Assets/Scripts/MapCave2/MinimapFollowMapCave2.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class MinimapFollowMapCave2 : MonoBehaviour
+{
+    public Transform target; // player
+    public Vector3 offset = new Vector3(0f, 0f, -10f);
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+        Vector3 newPos = target.position + offset;
+        transform.position = newPos;
+    }
+}
