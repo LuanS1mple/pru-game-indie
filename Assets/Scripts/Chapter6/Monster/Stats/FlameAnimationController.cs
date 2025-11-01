b3615035ae6070270b80447c83e9a5ff25910bfa
LuanS1mple <luans1mple2004@gmail.com>
2025-11-01 21:51:07 +0700

    update +1 boss skill

---
+++ Assets/Scripts/Chapter6/Monster/Stats/FlameAnimationController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameAnimationController : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb= GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Kiểm tra tag
+        if (collision.CompareTag("Player") || collision.CompareTag("Ground"))
+        {
+            animator.SetTrigger("IsBom");
+            // Ngừng mọi chuyển động
+            rb.velocity = Vector2.zero;
+            rb.angularVelocity = 0f;
+
+            // Tắt trọng lực
+            rb.gravityScale = 0f;
+
+            // Giữ nguyên vị trí, không rơi nữa
+            rb.bodyType = RigidbodyType2D.Kinematic;
+            Destroy(gameObject, 0.18f);
+        }
+    }
+}
