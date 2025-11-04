9ab34ed00fcc214ba9df34e97d3a94b7e3b54d9b
Doan Dat <156067447+dtdat2011@users.noreply.github.com>
2025-11-04 21:06:46 +0700

    add skill

---
+++ Assets/Assets/cavern1/scripts/BossWindEffect.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class BossWindEffect : MonoBehaviour
+{
+    public GameObject windLeft;
+    public GameObject windRight;
+    public float windDuration = 0.5f; // Hiệu ứng tồn tại 0.5s
+
+    // Gọi khi Boss tiếp đất
+    public void PlayWindEffect()
+    {
+        if (windLeft != null)
+        {
+            windLeft.SetActive(true);
+            StartCoroutine(DisableAfterTime(windLeft));
+        }
+
+        if (windRight != null)
+        {
+            windRight.SetActive(true);
+            StartCoroutine(DisableAfterTime(windRight));
+        }
+    }
+
+    private System.Collections.IEnumerator DisableAfterTime(GameObject obj)
+    {
+        yield return new WaitForSeconds(windDuration);
+        obj.SetActive(false);
+    }
+}
